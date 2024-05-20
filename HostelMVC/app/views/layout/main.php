6b69c7bd6d1f16759693f9483bcc3612f210cbf3
Al-Mahfuzh Fadhlur Rohman <124601324+almhfzrhmn@users.noreply.github.com>
2024-05-20 22:15:36 +0700

    Create main.php

---
+++ HostelMVC/app/views/layout/main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+</head>
+<body>
+    <?= $content ?>
+    <script src="/public/js/jquery.min.js"></script>
+    <script src="/public/js/bootstrap.min.js"></script>
+</body>
+</html>
